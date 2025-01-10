0eda25078399b7bb930c66c7c5ef22c28c0ff6c2
GVMNREDDY <113527971+GVMNREDDY@users.noreply.github.com>
2025-01-10 09:26:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import HoverEffectDiv from './Gift/divGiftAnimation';
+import Body_Background from './PortFolio/Body_Background';
+import HeaderFiles from './PortFolio/Header';
+
+function App() {
+  return (
+    <>
+    <HeaderFiles/>
+    <Body_Background/>
+    {/* <HoverEffectDiv/> */}
+    </>
+  );
+}
+
+export default App;
